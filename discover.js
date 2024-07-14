90d3df8a2fd17be103ac1f2d63218d4cdcc45946
timur4923 <139814300+timurko7674@users.noreply.github.com>
2024-07-14 15:20:59 +0100

    Create discover.js

---
+++ discover.js
@@ -0,0 +1,7 @@
+// discover.js
+
+function searchGames() {
+    const query = document.getElementById('search-bar').value;
+    // Logic to search for games
+    alert('Searching for: ' + query);
+}
